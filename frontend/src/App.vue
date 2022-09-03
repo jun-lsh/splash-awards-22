2b8193dea6dc7b6b9e988a150735a811444e50d4
jun <dosecomix@gmail.com>
2022-09-04 00:29:11 +0800

    vuetify boilerplate, now without the glitch

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {},
+});
+</script>
---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        <div v-if="name === ''">
+          Your profile
+        </div>
+        <div v-else>
+          Welcome to Vue, {{ name }}
+        </div>
+        <v-spacer/>
+      </v-card-title>
+      <div class="pa-8">
+        <v-text-field
+          v-model="name"
+          label="Name"
+        >
+        </v-text-field>
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Main",
+  data: function () {
+    return {
+      name: "",
+      mainOut: ""
+    };
+  },
+});
+</script>
