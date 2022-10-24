8063df91b1cf57d77f65dac97c1eca63bd31f1ca
mug1wara26 <aloysiusgsq@gmail.com>
2022-10-24 18:21:07 +0800

    working on populating clusters

--- frontend/src/views/Clusters.vue
+++ frontend/src/views/Clusters.vue
@@ -4,17 +4,16 @@
 
     <div style="max-width: 80rem">
       <v-row class="mt-4">
-        <v-col cols="12" lg="4" md="8" v-for="item in sortedClusters" :key="item.id">
+        <v-col cols="12" lg="4" md="8" v-for="(item, index) in sortedClusters" :key="index">
           <router-link
             class="unit-card"
             :to="'/home?lat=' + item.lat + '&lng=' + item.lng"
             style="text-decoration: none; color: inherit;"
           >
             <v-card class="pa-4 fill-height d-flex flex-column" elevation="2">
-              <v-card-title>#{{ item.id }}</v-card-title>
+              <v-card-title>Concentration: {{ item.conc }}</v-card-title>
               <v-card-text>
                 Lat, Long: {{ item.lat }}, {{ item.lng }}<br/>
-                Size: {{ item.conc }}
               </v-card-text>
             </v-card>
           </router-link>
@@ -26,33 +25,72 @@
 
 <script lang="ts">
 import Vue from "vue";
+import {Multihash} from "@/types/multihash";
+import ipfs_utils from "@/plugins/ipfs_decode";
+import Web3 from "web3";
+import {Contract} from "web3-eth-contract";
 export default Vue.extend({
   name: "Clusters",
-  computed: {
-    clusters(): Array<{
-      id: number,
-      lat: number,
-      lng: number,
-      conc : number
-    }> {
-      return [
-        {
-          id: 1,
-          lat: 10,
-          lng: 15,
-          conc: 100,
-        },
-      ];
-    },
-    sortedClusters(): Array<{
-      id: number,
-      lat: number,
-      lng: number,
-      conc : number
-    }> {
-        let clusters = this.clusters;
-        return clusters.sort((a,b) => a.conc - b.conc).slice(0,10);
+  data: () =>(
+    {
+      CONTRACT_ADDRESS: "0x75BCc6456812A005084391ADfBB21c6C54726db5",
+      iframe_created: false,
+      web3: {} as Web3,
+      eternalStorage: {} as Contract,
+      eternalStorageJson: require("./../components/EternalStorage.json"),
+      clusters: {} as Array<{lat: number, lng: number, conc: number}>,
     }
+  ),
+  computed: {
+    sortedClusters(): Array<{
+      lat: number,
+      lng: number,
+      conc : number
+    }> {
+      if (this.clusters !== {}) {
+        let clusters = this.clusters;
+        return clusters.sort((a, b) => a.conc - b.conc).slice(0, 10);
+      }
+    }
+  },
+  methods: {
+    getHeatMapData(timestamp : number,  coords : string) : Promise<any>{
+      console.log("called");
+      return new Promise<any>(
+        resolve => {
+          this.eternalStorage.methods.getHashValue(timestamp, coords).call().then(
+            function(multihash : Multihash){
+              let ipfs_hash = ipfs_utils.decode(multihash);
+              console.log(ipfs_hash);
+              fetch("https://cf-ipfs.com/ipfs/" + ipfs_hash, {
+                headers: {
+                  "accept": "application/json",
+                }
+              }).then(
+                response => response.json().then(
+                  data => {
+                    //console.log(data)
+                    resolve(data);
+                  }
+                )
+              );
+            }
+          );
+        }
+      );
+    }
+  },
+  mounted(){
+    this.web3 = new Web3(
+      new Web3.providers.HttpProvider(
+        `https://goerli.infura.io/v3/4fb5537dd7124137a2bc95668e973d76`
+      )
+    );
+    this.eternalStorage = new this.web3.eth.Contract(this.eternalStorageJson.abi, this.CONTRACT_ADDRESS);
+
+    this.getHeatMapData(120, 0x0).then(data => {
+      console.log(data);
+    });
   }
 });
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -147,7 +147,7 @@ export default Vue.extend({
         }
       );
         }
-      )
+      );
     }
   },
   mounted () {
